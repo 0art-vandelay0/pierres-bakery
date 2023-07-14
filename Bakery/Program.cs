a678b5ece51d40b596d7c4aed9c54f4e628604a6
Casey Hill <97810619+0art-vandelay0@users.noreply.github.com>
2023-07-14 08:44:07 -0700

    Stage cs files

---
+++ Bakery/Program.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+
+namespace Bakery.Models
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("---------------------------");
+            Console.WriteLine("□ □ □ Pierre's Bakery □ □ □");
+            Console.WriteLine("--------------------------- \n ");
+        }
+    }
+}
