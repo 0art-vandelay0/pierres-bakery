9e0e8b759f8de5cfc273cff1a625b3580c3340cd
Casey Hill <97810619+0art-vandelay0@users.noreply.github.com>
2023-07-14 13:58:29 -0700

    Add GetPastryInput and PrintCheckOut functions to main

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -10,9 +10,16 @@ namespace Bakery.Models
         {
             Intro();
             int breadQuantity = GetBreadInput();
+            int pastryQuantity = GetPastryInput();
 
-            // CalculateTotalPrice()
-            // PrintCheckOut()
+            Bread breadOrder = new Bread(breadQuantity);
+            Pastry pastryOrder = new Pastry(pastryQuantity);
+
+            int breadTotalPrice = breadOrder.BreadTotalPrice();
+            int pastryTotalPrice = pastryOrder.PastryTotalPrice();
+            int totalPrice = breadTotalPrice + pastryTotalPrice;
+
+            PrintCheckOut(breadQuantity, breadTotalPrice, pastryQuantity, pastryTotalPrice, totalPrice);
         }
 
         public static void Intro()
@@ -76,6 +83,72 @@ namespace Bakery.Models
                 Console.WriteLine("\n");
                 Console.ResetColor();
                 return GetBreadInput();
+            }
+        }
+
+        public static int GetPastryInput()
+        {
+            Console.WriteLine("Would you like to purchase pastry? y/n");
+            string pastryResponse = Console.ReadLine();
+            try
+            {
+                if (pastryResponse == "y" || pastryResponse == "Y")
+                {
+                    Console.WriteLine("How many pastries would you like?");
+                    int pastryQuantity = int.Parse(Console.ReadLine());
+                    return pastryQuantity;
+                }
+                else if (pastryResponse == "n" || pastryResponse == "N")
+                {
+                    return 0;
+                }
+                else
+                {
+                    throw new Exception();
+                }
+            }
+            catch (Exception)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("⚠  Error processing your input '" + pastryResponse + "' ⚠");
+                Console.WriteLine("Please enter a valid response of y or n.");
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Try again...");
+                Console.WriteLine("\n");
+                Console.ResetColor();
+                return GetPastryInput();
+            }
+        }
+        public static void PrintCheckOut(int breadQuantity, int breadTotalPrice, int pastryQuantity, int pastryTotalPrice, int totalPrice)
+        {
+            if (totalPrice == 0)
+            {
+                Console.WriteLine("-----------------------------");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("                             ");
+                Console.WriteLine("        – Receipt –          ");
+                Console.WriteLine("                             ");
+                Console.ResetColor();
+                Console.WriteLine("-----------------------------");
+                Console.WriteLine(" \u2049 No items were purchased. \u2049");
+            }
+            else
+            {
+                Console.WriteLine("-----------------------------");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("                             ");
+                Console.WriteLine("        – Receipt –          ");
+                Console.WriteLine("                             ");
+                Console.ResetColor();
+                Console.WriteLine("-----------------------------");
+                Console.WriteLine("Bread Quantity: " + breadQuantity);
+                Console.WriteLine("Bread Total Price: $" + breadTotalPrice);
+                Console.WriteLine("Pastry Quantity: " + pastryQuantity);
+                Console.WriteLine("Pastry Total Price: $" + pastryTotalPrice);
+                Console.WriteLine("Total Price: $" + totalPrice);
+                Console.WriteLine("-----------------------------");
+                Console.WriteLine("Thank you for your purchase!");
+                Console.WriteLine("-----------------------------");
             }
         }
     }
